e1aaab76a4ae913d4217490cb64833df213599a9
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-07-16 00:59:33 +0300

    updated the config file with the cluster and database user created on a cloud-hosted MongoDB service(MongoDB Atlas)

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://sereya123:sereya123@cluster0.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://sereya123:sereya123@cluster0.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://sereya123:sereya123@cluster0.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
+
+// mongodb+srv://sereya123:<password>@cluster0.sosohwn.mongodb.net/?retryWrites=true&w=majority
