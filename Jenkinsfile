311835c0d4cb32c3f365f5fa48e1caec7a63a559
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-07-16 14:57:06 +0300

    added a jenkins file and created a pipeline for automating build, test and deployment

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,10 @@
-pipeline {
-    agent any
-    stages {
-        
+pipeline { 
+  agent any
+  stages { 
+    stage('clone repository') {
+      steps { 
+        git 'https://github.com/sereyatiampati/gallery'
+      }
     }
+  }
 }
--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
+
+
+// Initializing the app
+const app = express();
+
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://sereya123:sereya123@cluster0.sosohwn.mongodb.net/?retryWrites=true&w=majority';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
@@ -19,10 +25,6 @@ let db = mongoose.connection;
 db.once('open', ()=>{
     console.log('Database connected successfully')
 })
-
-// Initializing the app
-const app = express();
-
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -44,3 +46,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
+module.exports = app;
